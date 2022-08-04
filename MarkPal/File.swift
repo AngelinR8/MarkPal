c82b155afff7cda1873eca525b8495b4612c3505
Angelin <angelinr21@gmail.com>
2022-08-03 20:50:28 -0400

    Initial Commit

---
+++ MarkPal/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  MarkPal
+//
+//  Created by Romieo, Angelin on 2022-07-27.
+//
+
+import Foundation
+
+func addCourse (courseName: String, termWeight: Int, finalWeight: Int){
+    
+}
---
+++ MarkPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarkPal
+//
+//  Created by Romieo, Angelin on 2022-07-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
